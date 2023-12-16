727ca369711ee0b5af8ba36e7f727fc4154adc49
AbenezerTZ <88774777+AbenezerTZ@users.noreply.github.com>
2023-12-16 12:50:37 +0300

    Time: 42 ms (27.34%), Space: 15.3 MB (11.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,16 +5,4 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        s=list(s)
-        t=list(t)
-        if len(s)!=len(t):
-            return False
-        else:
-            for i in s:
-                if i in t:
-                    t.remove(i)
-            if len(t)==0:
-                return True
-            else:
-                return False
-            
+        return sorted(list(s))==sorted(list(t))
