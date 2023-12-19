fffdceefc0c41228696ac8207e9ba0fae594ede8
AbenezerTZ <88774777+AbenezerTZ@users.noreply.github.com>
2023-12-19 19:49:14 +0300

    Time: 126 ms (34.60%), Space: 15 MB (27.97%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count={}
+        for i in nums:
+            if i in count:
+                count[i]+=1
+            elif i not in count:
+                count[i]=1
+        return max(count, key=count.get)
+
+        
