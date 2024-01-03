89578a2256d4802a0e5f39a52c072a6408126aaa
AbenezerTZ <88774777+AbenezerTZ@users.noreply.github.com>
2024-01-03 23:32:41 +0300

    Time: 24 ms (69.64%), Space: 13.5 MB (10.71%) - LeetHub

---
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def queensAttacktheKing(self, queens, king):
+        """
+        :type queens: List[List[int]]
+        :type king: List[int]
+        :rtype: List[List[int]]
+        """
+        ans = []
+        queens = {(i, j) for i, j in queens}
+        for d in [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]:
+          i = king[0] + d[0]
+          j = king[1] + d[1]
+          while 0 <= i < 8 and 0 <= j < 8:
+            if (i, j) in queens:
+              ans.append([i, j])
+              break
+            i += d[0]
+            j += d[1]
+        return ans
+
+        
+                
+            
+        
