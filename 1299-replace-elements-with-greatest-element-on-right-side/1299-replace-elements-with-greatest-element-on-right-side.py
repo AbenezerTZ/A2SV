a011b28108ae7914c7ee380d5201b21107f85e81
AbenezerTZ <88774777+AbenezerTZ@users.noreply.github.com>
2024-01-07 22:59:06 +0300

    Time: 546 ms (53.94%), Space: 15 MB (33.69%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def replaceElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        maxi = -1
+        for i in range(len(arr)-1,-1,-1):
+            temp = arr[i]
+            arr[i] = maxi
+            maxi = max(maxi,temp)
+        return arr
+        
