1ff5894f9160909dd541e207bec9f1a1fee53eb0
AbenezerTZ <88774777+AbenezerTZ@users.noreply.github.com>
2024-01-15 14:29:24 +0300

    Time: 114 ms (76.66%), Space: 14.6 MB (29.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,15 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        i=j=0
-        while i<len(nums) and j<len(nums):
-            if nums[i]==0 and nums[j]!=0:
-                nums[i],nums[j]=nums[j],nums[i]
-                i+=1
-            if nums[i]!=0:
-                i+=1
-            j+=1
-        return nums
-
-
-
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left],nums[right] = nums[right],nums[left]
+                left += 1
