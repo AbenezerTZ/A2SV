5dc82152e38f27f8f0045c4ea59c13bbeed67234
AbenezerTZ <88774777+AbenezerTZ@users.noreply.github.com>
2024-02-27 18:37:07 +0300

    Time: 327 ms (9.40%), Space: 18.6 MB (92.00%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -4,15 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        if len(nums)==1:
-            return []
-        nums.sort()
-        ans = []
         i = 0
-        j = 1
-        while j < len(nums):
-            if nums[i]==nums[j]:
+        ans = []
+        while i < len(nums):
+            if nums[i] == i+1 or nums[i]==nums[nums[i]-1]: 
+                i += 1
+            else:
+                nums[nums[i]-1], nums[i] = nums[i],nums[nums[i]-1]
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
                 ans.append(nums[i])
-            i += 1
-            j += 1
         return ans
