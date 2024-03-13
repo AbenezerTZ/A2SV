a6c0eb7f9f2eea5a562c741af753065e9b1a9276
AbenezerTZ <88774777+AbenezerTZ@users.noreply.github.com>
2024-03-13 22:05:32 +0300

    Time: 69 ms (10.48%), Space: 17.1 MB (12.18%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def applyOperations(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
         for i in range(len(nums)-1):
             if nums[i] == nums[i+1]:
                 nums[i] *= 2
@@ -13,4 +9,3 @@ class Solution(object):
                 nums.remove(nums[i])
                 nums.append(0)
         return nums
-            
