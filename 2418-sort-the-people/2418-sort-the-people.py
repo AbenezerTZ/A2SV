ee7c7c6a16bf96f5fe68073145016ea75946cd94
AbenezerTZ <88774777+AbenezerTZ@users.noreply.github.com>
2024-03-20 18:19:04 +0300

    Time: 875 ms (5.09%), Space: 17 MB (76.86%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,19 +1,15 @@
-class Solution(object):
-    def sortPeople(self, names, heights):
-        """
-        :type names: List[str]
-        :type heights: List[int]
-        :rtype: List[str]
-        """
-        pair = []
-        ans = []
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        pair = {}
         for i in range(len(names)):
-            temp = []
-            temp.append(heights[i])
-            temp.append(names[i])
-            pair.append(temp)
-        pair.sort(reverse = True)
-        for i in range(len(pair)):
-            ans.append(pair[i][1])
-        return ans
+            pair[heights[i]] = names[i]
+        for i in range(len(heights)):
+            for j in range(1,len(heights)):
+                if heights[j-1] < heights[j]:
+                    heights[j],heights[j-1] = heights[j-1],heights[j]
+                    
+        for n in range(len(heights)):
+            heights[n] = pair[heights[n]]
+        return heights
+            
         
