78283ea9092628db3a7402b2575687dbda0ed746
AbenezerTZ <88774777+AbenezerTZ@users.noreply.github.com>
2024-03-30 13:13:40 +0300

    Time: 169 ms (13.44%), Space: 18.2 MB (6.55%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -1,28 +1,26 @@
-class Solution(object):
-    def validMountainArray(self, arr):
-        """
-        :type arr: List[int]
-        :rtype: bool
-        """
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
         if len(arr) < 3:
             return False
+        elif arr[0] > arr[1]:
+            return False
         i = 0
-        while i < len(arr)-1:
+        flag = True
+        
+        while i < len(arr)-1 and flag:
             if arr[i] < arr[i+1]:
                 i += 1
-            elif arr[i] == arr[i+1]:
-                return False
+            elif arr[i] > arr[i+1]:
+                flag = False
             else:
-                break
-        if i+2==len(arr):
-            return True
-        if i+1==len(arr) or i == 0:
+                return False
+        if i == len(arr)-1:
             return False
-    
+        
         while i < len(arr)-1:
             if arr[i] > arr[i+1]:
                 i += 1
-            elif arr[i] == arr[i+1] or arr[i] < arr[i+1]:
+            else:
                 return False
         return True
-            
+                
