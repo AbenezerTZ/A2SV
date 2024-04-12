9d5e0b955e35e91146c450cdc7bac94b995a9dc1
AbenezerTZ <88774777+AbenezerTZ@users.noreply.github.com>
2024-04-12 18:27:05 +0300

    Time: 940 ms (40.94%), Space: 29.9 MB (68.43%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.py
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -1,27 +1,22 @@
-class Solution(object):
-    def maximumUniqueSubarray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
         freq = {}
-        i = j = 0
-        maxi = 0
-        temp = 0
-        while j < len(nums):
-            if nums[j] not in freq or freq[nums[j]] == 0:
-                freq[nums[j]] = 1
-                temp += nums[j]
-                maxi = max(maxi, temp)
-                j += 1
-            else:
-                while nums[i] != nums[j]: 
-                    temp -= nums[i]
+        maxi = nums[0]
+        i = 0
+        freq[nums[0]] = 1
+        temp = maxi
+        for j in range(1,len(nums)):
+            if nums[j] in freq and freq[nums[j]] == 0:
+                freq[nums[j]] += 1
+            elif nums[j] in freq and freq[nums[j]] == 1:
+                temp -= nums[j]
+                while nums[i] != nums[j]:
                     freq[nums[i]] -= 1
+                    temp -= nums[i]
                     i += 1
                 i += 1
-                j += 1
+            else:
+                freq[nums[j]] = 1
+            temp += nums[j]
+            maxi = max(maxi,temp)
         return maxi
-                
-                
-        
